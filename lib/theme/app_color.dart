389ea969def45d4e42404c9cf284b51a9d393452
GAJAHGAJAH <1123150165@global.ac.id>
2025-11-28 09:27:28 +0700

    membuat tema

---
+++ lib/theme/app_color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+ class AppColors {
+ static Color white = const Color(0xFFFCFCFC);
+ static Color black = const Color(0xFF161616);
+ static Color orange = const Color(0xFFF6AB53);
+ }
